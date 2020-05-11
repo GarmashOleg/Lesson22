5bf9c9a4e0a54c87e9b6942e1c0111eab86906a3
Oleh Harmash <oleh_harmash@epam.com>
2020-05-11 19:55:35 +0300

    it works

---
+++ Lesson22-threads/Lesson22-threads/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+
+namespace Lesson22_threads
+{
+    public class Program
+    {
+        readonly object locker = new object();
+        bool isDone;
+
+        public static void Main(string[] args)
+        {
+            var testExample = new Program();
+
+            Thread.CurrentThread.Name = "Main";
+
+
+            //second thread
+            var secondThread = new Thread(testExample.PrintNumbers);
+            secondThread.Name = "second thread";
+            secondThread.Start();
+
+            var threadThread = new Thread(testExample.PrintNumbers);
+            threadThread.Name = "third thread";
+            threadThread.Start();
+
+            testExample.PrintNumbers();
+        }
+
+        public void PrintNumbers()
+        {
+            //Console.WriteLine($"Current thread: " + Thread.CurrentThread.Name);
+
+            lock (locker)
+            {
+
+
+                for (int i = 0; i < 10; i++)
+                {
+                    if (!isDone)
+                    {
+                        Console.WriteLine($"Value {i} was created by {Thread.CurrentThread.Name}");
+                        Console.WriteLine($"Value {i} was finished {Thread.CurrentThread.Name}");
+                        isDone = false;
+                    }
+
+                }
+            }
+        }
+    }
+}
